62c18d524c406a6cc276252dab64dca3ea7c723c
daiki_matsumoto <64685202+daiki14@users.noreply.github.com>
2022-09-18 23:45:47 +0900

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,354 @@
+
+
+<?php include("./header.php") ?>
+
+
+
+
+
+
+
+<!-- ↓main -->
+<main class="main">
+      <!-- fv -->
+      <section class="fv">
+        <div class="fv__inner">
+          <div class="fv__inner-slider">
+            <ul>
+              <li>
+                <picture>
+                  <source media="(min-width: 768px)" srcset="img/fv_bg1-pc.jpg">
+                  <img src="img/fv_bg1-sp.jpg" alt=""/>
+                </picture>
+              </li>
+              <li>
+                <picture>
+                  <source media="(min-width: 768px)" srcset="img/fv_bg2-pc.jpg">
+                  <img src="img/fv_bg2-sp.jpg" alt=""/>
+                </picture>
+              </li>
+            </ul>
+          </div>
+          <h2 class="fv__inner-ttl">
+            一人一人に向き合い<br />健康までの導きをサポート
+          </h2>
+          <img src="img/fv_bg.png" alt="ロゴ" />
+        </div>
+      </section>
+      <!-- /.fv -->
+
+      <!-- about -->
+      <section id="about" class="about">
+        <div class="about__inner inner">
+          <h3 class="subttl">木をメインに創られた<br class="_sp">落ち着いた空間</h3>
+          <ul class="about__inner-list">
+            <li>
+              <img src="img/about_img1.jpg" alt="" />
+            </li>
+            <li>
+              <img src="img/about_img2.JPG" alt="" />
+            </li>
+            <li>
+              <img src="img/about_img3.JPG" alt="" />
+            </li>
+          </ul>
+          <p>
+            東洋医学を基に手技のみで筋肉•骨格•神経に働きかけ、<br class="_pc-tab">お身体のお悩みに合った施術を提供します。<br />
+            <br />
+            ”美”と”健康”を”芯”から引き出すことで毎日が楽しく健康に過ごせるよう整え、<br class="_pc-tab">自然治癒力を高める事で軸のある身体へと導きます。<br />
+            <br />
+            痛みがあるわけではないけど常に肩が重い、<br class="_sp">頭痛や疲れが取れない、<br class="_pc-tab">同じ姿勢を続けるのが辛い、<br class="_sp">朝起きると首が回らない、運動能力が落ちた、<br>などそうならない為にもお悩みの原因を解消する事で、<br class="_pc-tab">痛みや不調を繰り返さない健康的な身体づくりをサポートします。
+          </p>
+        </div>
+      </section>
+      <!-- /.about -->
+
+      <!-- concept -->
+      <section class="concept">
+        <div class="concept__inner inner">
+          <h3 class="subttl">Body Arrange 軸の技と空間</h3>
+          <div class="concept__inner-img">
+            <ul>
+              <li class="left">
+                <p>
+                  整体の手技をもとに行うお顔の施術。<br>
+                  お顔の筋肉をゆるめ血流を促進させる事で<br>しわ、むくみ、たるみの改善。<br>
+                  若々しい肌を取り戻し小顔をつくります。<br>
+                  男性にも人気の施術です。
+                </p>
+              </li>
+              <li class="right">
+                <p>
+                  Body Arrange 軸は駅から通いやすいお店を<br>目指して出店しております。<br>
+                  落ち着いた空間を感じていただく為、<br>内装も清潔・除菌も気を配って、<br class="_sp">場のご提供に努めております。
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!-- /.concept -->
+      <!-- service -->
+      <section id="service" class="service">
+        <div class="service__inner inner">
+          <h3 class="subttl">施術内容・料金</h3>
+          <div class="service__inner-table">
+            <dl>
+              <dt>改善整体</dt>
+              <dd>
+                <p class="time">約50分</p>
+                <p class="price">
+                  ¥6,900円
+                  <span>(税込)</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>改善整体</dt>
+              <dd>
+                <p class="time">約25分</p>
+                <p class="price">
+                  ¥3,700円
+                  <span>(税込)</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>小顔改善</dt>
+              <dd>
+                <p class="time">約15分</p>
+                <p class="price">
+                  ¥1,700円
+                  <span>(税込)</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>頭ほぐし</dt>
+              <dd>
+                <p class="time">約15分</p>
+                <p class="price">
+                  ¥1,700円
+                  <span>(税込)</span>
+                </p>
+              </dd>
+            </dl>
+            <dl>
+              <dt>ふくらはぎ</dt>
+              <dd>
+                <p class="time">約15分</p>
+                <p class="price">
+                  ¥1,700円
+                  <span>(税込)</span>
+                </p>
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </section>
+      <!-- /.service -->
+      
+      <!-- gallery -->
+      <section class="gallery">
+        <div class="gallery__inner">
+          <div class="gallery__inner-slider">
+            <ul>
+              <li>
+                <img src="img/slider1.jpg" alt="" />
+              </li>
+              <li>
+                <img src="img/slider2.jpg" alt="" />
+              </li>
+              <li>
+                <img src="img/slider3.jpg" alt="" />
+              </li>
+              <li>
+                <img src="img/slider4.jpg" alt="" />
+              </li>
+              <li>
+                <img src="img/slider5.jpg" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!-- /.gallery -->
+
+      <!-- information -->
+      <section id="information" class="information">
+        <div class="information__inner inner">
+          <div class="information__inner-list">
+            <ul>
+              <li class="left">
+                <h3 class="subttl">INFORMATION</h3>
+                <div class="table">
+                  <dl>
+                    <a href="#">
+                      <dt><time>2022.7.25</time></dt>
+                      <dd><h4>ここにタイトルが入ります。</h4></dd>
+                    </a>
+                  </dl>
+                  <dl>
+                    <a href="#">
+                      <dt><time>2022.7.25</time></dt>
+                      <dd><h4>ここにタイトルが入ります。</h4></dd>
+                    </a>
+                  </dl>
+                  <dl>
+                    <a href="#">
+                      <dt><time>2022.7.25</time></dt>
+                      <dd><h4>ここにタイトルが入ります。</h4></dd>
+                    </a>
+                  </dl>
+                  <dl>
+                    <a href="#">
+                      <dt><time>2022.7.25</time></dt>
+                      <dd><h4>ここにタイトルが入ります。</h4></dd>
+                    </a>
+                  </dl>
+                  <dl>
+                    <a href="#">
+                      <dt><time>2022.7.25</time></dt>
+                      <dd><h4>ここにタイトルが入ります。</h4></dd>
+                    </a>
+                  </dl>
+                </div>
+                <div class="information__inner-btn">
+                  <a href="news.html">MORE</a>
+                </div>
+              </li>
+              <li class="right">
+                <h3 class="subttl">OFFICIAL SNS</h3>
+                <div class="list">
+                  <ul>
+                    <li>
+                      <a href="https://www.instagram.com/y.to.shi.ki/?hl=ja">INSTAGRAM</a>
+                    </li>
+                    <li>
+                      <a href="#">LINE</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!-- /.information -->
+
+      <!-- qa -->
+      <section id="qa" class="qa">
+        <div class="qa__inner inner">
+          <h3 class="subttl">Q&A</h3>
+          <div class="qa__inner-table">
+            <dl>
+              <dt>
+                <span>Q1.</span>
+                <p>施術は痛いですか？</p>
+              </dt>
+              <dd>お身体の状態が良くなければ痛みを伴いますが状態が良くなれば痛みは和らぎます。</dd>
+            </dl>
+            <dl>
+              <dt>
+                <span>Q2.</span>
+                <p>健康保険の適用はできますか？</p>
+              </dt>
+              <dd>整体は民間療法であり、医師による保険診療ではない為、健康保険は適用されません。</dd>
+            </dl>
+            <dl>
+              <dt>
+                <span>Q3.</span>
+                <p>医療費控除の対象ですか？</p>
+              </dt>
+              <dd>医療費控除の対象ではございません。</dd>
+            </dl>
+            <dl>
+              <dt>
+                <span>Q4.</span>
+                <p>どんな服装で行けばよいですか？</p>
+              </dt>
+              <dd>動きやすく楽な服装がおすすめです。着替えのご用意がございませんのでご自身でお持ちいただくようお願い致します。</dd>
+            </dl>
+            <dl>
+              <dt>
+                <span>Q5.</span>
+                <p>ご予約とキャンセルについて</p>
+              </dt>
+              <dd>お客様の大切なお時間の中で丁寧な施術を心がけている為、完全予約制となります。キャンセルなさる場合は、速やかにご連絡を頂けますようお願い致します。また、大幅な遅刻を繰り返す方・当日キャンセルを繰り返す方はご予約をお断りする場合がございます。</dd>
+            </dl>
+          </div>
+        </div>
+      </section>
+      <!-- /.qa -->
+      <!-- access -->
+      <section id="access" class="access">
+        <div class="access__inner inner">
+          <h3 class="subttl">ACCESS</h3>
+          <div class="access__inner-list">
+            <ul>
+              <li class="left">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.363957047415!2d135.19255941510016!3d34.69599899097792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008ee5a47c7b43%3A0xb2b5c14469adf08b!2z44CSNjUxLTAwOTQg5YW15bqr55yM56We5oi45biC5Lit5aSu5Yy655C044OO57eS55S677yV5LiB55uu77yV4oiS77yZ!5e0!3m2!1sja!2sjp!4v1661759105871!5m2!1sja!2sjp"
+                  style="border: 0"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </li>
+              <li class="right">
+                <h4>Body Arrange 軸</h4>
+                <dl>
+                  <dt>営業時間</dt>
+                  <dd>
+                    平日9:00~18:00<br />
+                    土日9:00~15:00<br />
+                    日・祝休み
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>住所</dt>
+                  <dd>
+                    〒651-0094 神戸市中央区琴ノ緒町5丁目5-9フューチャービル401
+                  </dd>
+                </dl>
+                <dl>
+                  <dt>アクセス</dt>
+                  <dd>
+                    <div>
+                      <strong>●電車でお越しの方</strong>
+                      <p>
+                        ここに電車でお越しの方のテキストを入れます。ここに電車でお越しの方のテキストを入れます。ここに電車でお越しの方のテキストを入れます。
+                      </p>
+                    </div>
+                    <div>
+                      <strong>●バスでお越しの方</strong>
+                      <p>
+                        ここにバスでお越しの方のテキストを入れます。ここにバスでお越しの方のテキストを入れます。ここにバスでお越しの方のテキストを入れます。
+                      </p>
+                    </div>
+                    <div>
+                      <strong>●お車でお越しの方</strong>
+                      <p>
+                        ここにお車でお越しの方のテキストを入れます。ここにお車でお越しの方のテキストを入れます。ここにお車でお越しの方のテキストを入れます。
+                      </p>
+                    </div>
+                  </dd>
+                </dl>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!-- /.access -->
+    </main>
+    <!-- ↑main -->
+
+
+
+
+
+
+
+
+
+    <?php include("footer.php") ?>
---
+++ single.php
@@ -0,0 +1,73 @@
+<?php include("./header.php") ?>
+
+
+<!-- main -->
+<main id="single" class="main">
+  <!-- hero -->
+  <section class="hero">
+    <div class="hero__inner">
+      <div class="text">
+        <h2>ホームページ公開しました。</h2>
+      </div>
+    </div>
+  </section>
+  <!-- /.hero -->
+
+  <!-- breadcrumb -->
+  <section class="breadcrumb">
+    <div class="breadcrumb__inner inner">
+      <ul>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">お知らせ</a>
+        </li>
+        <li>
+          ホームページ公開しました。
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- /.breadcrumb -->
+
+
+  <!-- content -->
+  <section class="content">
+    <div class="content__inner inner">
+      <h3>ホームページ公開しました。</h3>
+      <span>2022.09.03</span>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+
+      <!-- pager -->
+      <div class="pager">
+        <ul>
+          <li>
+            <a href="#">＜＜ PREV</a>
+          </li>
+          <li>
+            <a href="#">一覧へ</a>
+          </li>
+          <li>
+            <a href="#">NEXT ＞＞</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /.pager -->
+    </div>
+  </section>
+  <!-- /.content -->
+
+
+
+</main>
+<!-- /.main -->
+
+
+
+
+
+<?php include("./footer.php") ?>
